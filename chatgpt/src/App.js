5a75df33bdc20557e3b084942378e924390e8748
harshagudem <157737492+harshagudem@users.noreply.github.com>
2024-08-03 16:39:22 -0600

    frontend part 1

---
+++ chatgpt/src/App.js
@@ -0,0 +1,21 @@
+
+
+
+function App() {
+  return (
+    <div className="app">
+      <section className="side-nav">
+        <button>+ New Chat</button>
+        <ul className="history">
+          <li>Harsha</li>
+        </ul>
+        <nav>
+          <p>Made by Harsha</p>
+        </nav>
+      </section>
+      <section className="main"></section>
+    </div>
+  );
+}
+
+export default App;
